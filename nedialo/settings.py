ed31a4c14e294c6c4ed082e5bb3314d644cdc596
Ahmed Ezzat <ahmedezzathagag@gmail.com>
2024-10-07 16:08:29 +0300

    launch

--- nedialo/settings.py
+++ nedialo/settings.py
@@ -85,13 +85,25 @@ WSGI_APPLICATION = 'nedialo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nedialo',
+        'USER': 'zeo',
+        'PASSWORD': 'ahmedezzat001qq',
+        'HOST': 'localhost',  # Or use the IP of your MySQL server
+        'PORT': '3306',       # Default MySQL port
+    }
+}
 
 
 # Password validation
